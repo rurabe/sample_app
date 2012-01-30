05e22764c0f1922322e1cdf0dd682be0790f9f99
Ryan Urabe <rurabe@gmail.com>
2012-01-29 21:38:27 -0800

    finished creation of user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,17 @@ class UsersController < ApplicationController
   
   def new
     @title= "Sign up"
+    @user = User.new
   end
-
-
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to Shitty Twitter"
+      redirect_to @user
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
+  end
 end
