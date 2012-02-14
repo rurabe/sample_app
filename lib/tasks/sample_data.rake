b2d7c1e3ffead2c1313e6f9e59350eb9df6b8fed
Ryan Urabe <rurabe@gmail.com>
2012-02-13 22:12:38 -0800

    finished chapter 12

--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -1,14 +1,19 @@
 namespace :db do
   desc "Fill database with sample data"
-  
   task :populate => :environment do
-    Rake::Task['db:reset'].invoke
+      Rake::Task['db:reset'].invoke
+      make_users
+      make_microposts
+      make_relationships
+    end
+  end
+  
+  def make_users
     admin = User.create!(:name => "Ryan Urabe",
                  :email => "rurabe@gmail.com",
                  :password => "omega77",
                  :password_confirmation => "omega77")
     admin.toggle!(:admin)
-  
     99.times do |n|
       name  = Faker::Name.name
       email = "example-#{n+1}@railstutorial.org"
@@ -17,12 +22,23 @@ namespace :db do
                    :email => email,
                    :password => password,
                    :password_confirmation => password)
-    
+    end
+  end
+  
+  def make_microposts  
     50.times do
       User.all(:limit => 6).each do |user|
         user.microposts.create!(:content => Faker::Lorem.sentence(5))     
-    end         
-    end
+      end         
     end
   end
-end
+
+  def make_relationships
+    users = User.all
+    user = users.first
+    following = users[1..50]
+    followers = users[3..40]
+    following.each { |followed| user.follow!(followed) }
+    followers.each { |follower| follower.follow!(user) }
+  end
+
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -11,17 +11,44 @@ describe PagesController do
   end
 
   describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title",
+    
+    describe "when not signed in" do
+      
+      before(:each) do
+        get :home
+      end
+    
+      it "should be successful" do
+        get 'home'
+        response.should be_success
+      end
+    
+      it "should have the right title" do
+        get 'home'
+        response.should have_selector("title",
                                     :content => @base_title +" | Home")
+      end
     end
-  end
   
+    describe "when signed in" do
+      
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        other_user = Factory(:user, :email => Factory.next(:email))
+        other_user.follow!(@user)
+      end
+        
+        it "should have the right follower/following counts" do
+          get :home
+          response.should have_selector("a",    :href => following_user_path(@user),
+                                                :content => "0 following")
+          response.should have_selector("a",    :href => followers_user_path(@user),
+                                                :content => "1 follower")
+        end
+    end
+  
+  
+  end
 
   describe "GET 'contact'" do
     it "should be successful" do
@@ -34,17 +61,19 @@ describe PagesController do
                                         :content => @base_title +" | Contact")
      end
   end
+  
   describe "Get 'about'" do
     it "should be successful" do
         get 'about'
         response.should be_success
-      end
+    end
+    
     it "should have the right title" do
           get 'about'
           response.should have_selector("title",
                                         :content => @base_title +" | About")
     end
-    end
+  end
     
   describe "Get 'help'" do
     it "should be successful" do
@@ -59,4 +88,4 @@ describe PagesController do
   end
 
   
-  end
+end
